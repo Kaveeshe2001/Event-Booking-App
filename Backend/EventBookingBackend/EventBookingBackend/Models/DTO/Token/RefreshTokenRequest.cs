4a9b4a16165c137c49376c9120d1291cea0ac889
Kaveesha <kaveeshewaduge111@gmail.com>
2024-12-02 12:56:57 +0530

    Implement RefreshTokenController

---
+++ Backend/EventBookingBackend/EventBookingBackend/Models/DTO/Token/RefreshTokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EventBookingBackend.Models.DTO.Token
+{
+    public class RefreshTokenRequest
+    {
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
--- Backend/EventBookingBackend/EventBookingBackend/obj/Debug/net8.0/EventBookingBackend.AssemblyInfo.cs
+++ Backend/EventBookingBackend/EventBookingBackend/obj/Debug/net8.0/EventBookingBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventBookingBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10091c07949fca7c285151f4ba7c62a857bc36db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882ce876d667c987e0469c8dbbbd2dc094604f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventBookingBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventBookingBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
